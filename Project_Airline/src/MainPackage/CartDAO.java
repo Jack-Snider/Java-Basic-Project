294582752b8aac4eb524d5e6c3b702557e64e688
Jack Snider <choiman2021@gmail.com>
2022-06-17 10:08:26 +0900

    Flight, Cart class added

---
+++ Project_Airline/src/MainPackage/CartDAO.java
@@ -0,0 +1,20 @@
+package MainPackage;
+
+public class CartDAO extends DBConnection{
+
+	
+	// DB연결 
+	public void accessDB() {
+		DBConnection.getConnection();
+	}
+	
+	
+	// 상품(티켓)을 장바구니에 담음
+	public void addTicket() {
+		
+	}
+	
+	
+	
+	
+}
---
+++ Project_Airline/src/MainPackage/FlightDTO.java
@@ -0,0 +1,5 @@
+package MainPackage;
+
+public class FlightDTO extends DBConnection{
+
+}
